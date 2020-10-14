d972770312f02be3ed571593cbe6b727700ef344
Animesh241100 <24animesh11@gmail.com>
2020-10-14 13:15:23 +0530

    Add Multiply feature

--- hello.c
+++ hello.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 
+void Multiply(int * arr, int n);
+
 int main() {
+  int arr[10] = {1, 2, 3, 4, 5, 6, 7, 6, 9, 10};
+  Multiply(arr, 10);
+  for (int i = 0; i < 10; i++)
+    printf("%d ", arr[i]);
   return 0;
+}
+
+void Multiply(int * arr, int n) {
+  for (int i = 0; i < n; i++)
+    arr[i] = 2 * arr[i];
 }
