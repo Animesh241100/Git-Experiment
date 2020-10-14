baa1026a78f91f69f2019dee972d8e61084dd7f1
Animesh241100 <24animesh11@gmail.com>
2020-10-14 16:40:27 +0530

    Make dummy changes

--- hello.c
+++ hello.c
@@ -5,6 +5,7 @@ void Multiply(int * arr, int n);
 int main() {
   int arr[10] = {1, 2, 3, 4, 5, 6, 7, 6, 9, 10};
   Multiply(arr, 10);
+  printf("hello w");
   for (int i = 0; i < 10; i++)
     printf("%d ", arr[i]);
   return 0;
