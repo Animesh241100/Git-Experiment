07991917e62e88db3d059919754884a7fbe59b62
Animesh241100 <24animesh11@gmail.com>
2020-10-14 13:02:18 +0530

    Add I/O Function

--- hello.c
+++ hello.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
 
+int Array_in(int * arr, int n);
+int Array_out(int * arr, int n);
+
 int main() {
+  int arr[10];
+  Array_in(arr, 10);
+  Array_out(arr, 10);
   return 0;
 }
+
+int Array_in(int * arr, int n) {
+  for (int i = 0; i < n; i++)
+    scanf("%d", &arr[i]);
+} 
+
+int Array_out(int * arr, int n) {
+  for (int i = 0; i < n; i++)
+    printf("%d ", arr[i]);
+  printf("\n");
+} 
